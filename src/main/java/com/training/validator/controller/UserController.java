f9651f6fe6dcf3052356b7238aa82f23cb497ded
eugenia.cemartan <eugenia.cemartan@itsmartsystems.eu>
2019-06-03 15:55:31 +0300

    Changes to UserController

--- src/main/java/com/training/validator/controller/UserController.java
+++ src/main/java/com/training/validator/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.training.validator.controller;
 
+import com.training.validator.beans.StatusCreate;
 import com.training.validator.beans.User;
+import com.training.validator.validators.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    UserValidator userValidator;
+
     @PostMapping(value = "/create",
             produces = {MediaType.APPLICATION_JSON_VALUE},
             consumes = {MediaType.APPLICATION_JSON_VALUE}
     )
-    public ResponseEntity<Void> create(
-        @RequestBody User user){
+    public ResponseEntity<StatusCreate> create(@RequestBody User user){
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        StatusCreate response = new StatusCreate();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
